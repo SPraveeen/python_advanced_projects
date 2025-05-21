983bedf657edc1c9b5e0d1d5b13f1596f792811b
SPraveeen <praveenji8498@gmail.com>
2025-05-21 17:43:49 +0530

    qr project

---
+++ QR_Code_Generator/qr_code.py
@@ -0,0 +1,3 @@
+import qrcode as qr
+img= qr.make("youtube.com/watch?v=fyre99dZQ8Y")
+img.save("andrew_tate_youtube.png")
---
+++ QR_Code_Generator/qr_code1.py
@@ -0,0 +1,11 @@
+import qrcode
+from PIL import Image
+import qrcode.constants
+
+qr=qrcode.QRCode(version=1,
+                 error_correction=qrcode.constants.ERROR_CORRECT_H,
+                 box_size=10,border=4)
+qr.add_data("youtube.com/watch?v=fyre99dZQ8Y")
+qr.make(fit=True)
+img=qr.make_image(fill_color="pink",back_color="lavender")
+img.save("Andrew_video.png")
